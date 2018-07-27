b9a33ff592f5b7ea3d0c2c0eead3399c13d1b0d4
pcdinh <pcdinh@gmail.com>
2018-07-27 17:12:14 +0700

    Fixed: rapidjson is unable to serialize datetime object without specifying datetime_mode

--- revopy/helpers/response_utils.py
+++ revopy/helpers/response_utils.py
@@ -8,7 +8,8 @@ class JsonResponse(Response):
 
     def __init__(self, content: object, status_code: int = 200, headers: dict = None, cookies: list = None):
         self.status_code = status_code
-        self.content = rapidjson.dumps(content).encode()
+        mode = rapidjson.DM_ISO8601 | rapidjson.DM_NAIVE_IS_UTC
+        self.content = rapidjson.dumps(content, datetime_mode=mode).encode()
         self.headers = headers or {}
         self.headers['Content-Type'] = 'application/json'
         self.cookies = cookies or []
